aff48709f49c48f3334891198672cee9fbe9ee43
agnesindah3 <agnesindah3@gmail.com>
2023-02-26 19:38:53 +0700

    upload

---
+++ index.js
@@ -0,0 +1,12 @@
+describe('Test', () => {
+	beforeEach(() => {
+		console.log('')
+	})
+
+	it('Bart should hear his mother', () => {
+		/** Test cases */
+	})
+	it('Lisa should hear her mother', () => {
+		/** Test cases */
+	})
+})
